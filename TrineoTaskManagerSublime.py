1170a5b3ae6d10bf749fc0a6602c235d4ebabcb6
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-03-13 10:20:40 +1300

    Store session Id in package settings

--- TrineoTaskManagerSublime.py
+++ TrineoTaskManagerSublime.py
@@ -5,10 +5,12 @@ import sublime, sublime_plugin, os.path, os, re, sys
 
 class ConnectToSalesforceCommand(sublime_plugin.WindowCommand):
     def run(self):
+        self.settings = sublime.load_settings("TrineoTaskManager.sublime-settings")
         self.window = self.window
         self.window.show_input_panel('Current Salesforce session ID', '', self.setSessionId, None, None)
 
     def setSessionId(self, id):
         self.id = id
-        # print("Salesforce session ID saved")
+        self.settings.set("sessionId", self.id)
+        print("Salesforce session ID saved")
 
