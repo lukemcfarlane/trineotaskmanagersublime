320c38238a41627bac6c230026436c2c77064abf
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-03-13 10:12:07 +1300

    Show input panel for user to enter session Id

--- TrineoTaskManagerSublime.py
+++ TrineoTaskManagerSublime.py
@@ -4,6 +4,11 @@
 import sublime, sublime_plugin, os.path, os, re, sys
 
 class ConnectToSalesforceCommand(sublime_plugin.WindowCommand):
-    def run(self, suffix):
-        # TODO
+    def run(self):
+        self.window = self.window
+        self.window.show_input_panel('Current Salesforce session ID', '', self.setSessionId, None, None)
+
+    def setSessionId(self, id):
+        self.id = id
+        # print("Salesforce session ID saved")
 
