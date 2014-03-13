06ee4148fe3a1b73d54c337f034aca508b47f44c
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-03-13 14:07:26 +1300

    Added new command to show current timer (unfinished)

--- TrineoTaskManagerSublime.py
+++ TrineoTaskManagerSublime.py
@@ -14,3 +14,18 @@ class ConnectToSalesforceCommand(sublime_plugin.WindowCommand):
         self.settings.set("sessionId", self.id)
         print("Salesforce session ID saved")
 
+
+class ShowCurrentTimeCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        self.settings = sublime.load_settings("TrineoTaskManager.sublime-settings")
+        if self.settings is not None:
+            self.sessionId = self.settings.get("sessionId")
+            if self.sessionId == "":
+                if sublime.ok_cancel_dialog("Uh oh, your Salesforce session ID has not been set, would you like to set it now?"):
+                    self.window.run_command("connect_to_salesforce")
+            else:
+                # self.window.active_view().set_status("z_current_timer", "(timer will show here)")
+                sublime.status_message("(timer will show here)")
+
+
+
